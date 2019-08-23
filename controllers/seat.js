3008c98e47f8f45d9012ae535edb713b39558bef
friskaawr <zahfriska@gmail.com>
2019-08-23 16:42:55 +0700

    edit controller create

--- controllers/seat.js
+++ controllers/seat.js
@@ -1,4 +1,28 @@
+const Seat = require('../models/user.js')
+
 class Controller {
+    static create(req, res){
+        const {block, index} = req.body
+        const newSeat = {block, index}
+        Seat.create(newSeat)
+            .then(create=> {
+                res.status(201).json(create)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message : err
+                })
+            })  
+    }
+
+    static fetchData(req, res){
+        const id = req.params.id
+        Seat.findById
+    }
+
+    static updateStatusSeat(req, res){
+
+    }
     static test(req, res) {
         res.status(200).json({
             message: 'Seat route example'
--- routes/user.js
+++ routes/user.js
@@ -2,5 +2,7 @@ const router = require('express').Router()
 const Controller = require('../controllers/user')
 
 router.get('/test', Controller.test)
+router.post('/login', Controller.login)
+router.post('/register', Controller.register)
 
 module.exports = router
