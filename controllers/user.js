70741dab2bb833c8faf8c99ae089b47f32528148
Stephen Lie <stephenlie55@yahoo.com>
2019-08-23 16:23:47 +0700

    Login/register

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,59 @@
+const User = require('../models/user')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 class Controller {
     static test(req, res) {
         res.status(200).json({
             message: 'User route example'
+        })
+    }
+
+    static login(req, res) {
+        if (req.body.nik) {
+            User.findOne({
+                nik: req.body.nik
+            })
+            .then( (user) => {
+                if (user) {
+                    if (bcrypt.compareSync(req.body.password, user.password)) {
+                        const token = jwt.sign({ nik: user.nik, id: user.id})
+                        res.status(200).json({
+                            token
+                        })
+                    } else {
+                        console.log("Error password doesn't match")
+                        res.status(400).json({
+                            message: 'Unidentified NIK / Password'
+                        })
+                    }
+                } else {
+                    console.log('Error NIK not found')
+                    res.status(400).json({
+                        message: 'Unidentified NIK / Password'
+                    })
+                }
+            })
+        } else {
+            res.status(400).json({
+                message: 'NIK is required and must be filled'
+            })
+        }
+    }
+    
+    static register(req, res) {
+        User.create({
+            name: req.body.name,
+            nik: req.body.nik,
+            password: req.body.password
+        })
+        .then( (user) => {
+            res.status(200).json(user)
+        })
+        .catch( (err) => {
+            res.status(err.code).json({
+                message: err.message
+            })
         })
     }
 }
